b40e69d0841d5b9d18e397dbc883df9bb8957dff
yc_ren <yc_ren@wantexe.com>
2022-09-09 18:17:43 +0800

    p39

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const os = require('os')
+// cpu核数
+const threads = os.cpus().length;
 
 module.exports = {
     entry: './src/main.js',
@@ -13,53 +16,74 @@ module.exports = {
     module: {
         rules: [
             {
-                // 用来匹配 .css 结尾的文件
-                test: /\.css$/,
-                // use 数组里面 Loader 执行顺序是从右到左
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader',
-                ],
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-            {
-                test: /\.(png|jpg?g|gif|webp|svg)$/,
-                type: 'asset',
-                parser: {
-                 dataUrlCondition: {
-                   maxSize: 20 * 1024 // 小于20kb，转base64  
-                 }
-                },
-                generator: {
-                    filename: 'image/[hash:10][ext][query]'   //hash:10  取哈希值前10位
-                }
-            },
-            {
-                test: /\.(ttf|woff2?|map3|map4|avi)$/,
-                type: 'asset/resource',    //纯输出这些资源，不需要做转换
-                generator: {
-                    filename: 'media/[hash:10][ext][query]'   //hash:10  取哈希值前10位
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/, // 排除node_modules代码不编译
-                loader: "babel-loader",
+                oneOf: [
+                    {
+                        // 用来匹配 .css 结尾的文件
+                        test: /\.css$/,
+                        // use 数组里面 Loader 执行顺序是从右到左
+                        use: ["style-loader", "css-loader"],
+                    },
+                    {
+                        test: /\.less$/,
+                        use: [
+                            'style-loader',
+                            'css-loader',
+                            'less-loader',
+                        ],
+                    },
+                    {
+                        test: /\.s[ac]ss$/,
+                        use: ["style-loader", "css-loader", "sass-loader"],
+                    },
+                    {
+                        test: /\.(png|jpg?g|gif|webp|svg)$/,
+                        type: 'asset',
+                        parser: {
+                            dataUrlCondition: {
+                                maxSize: 20 * 1024 // 小于20kb，转base64  
+                            }
+                        },
+                        generator: {
+                            filename: 'image/[hash:10][ext][query]'   //hash:10  取哈希值前10位
+                        }
+                    },
+                    {
+                        test: /\.(ttf|woff2?|map3|map4|avi)$/,
+                        type: 'asset/resource',    //纯输出这些资源，不需要做转换
+                        generator: {
+                            filename: 'media/[hash:10][ext][query]'   //hash:10  取哈希值前10位
+                        }
+                    },
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/, // 排除node_modules代码不编译,因为已经编译了
+                        use:[
+                            {
+                                loader:'thread-loader',
+                                options:{
+                                    works:threads
+                                }
+                            },
+                            {
+                                loader: "babel-loader",
+                                options: {
+                                    cacheDirectory: true, // 开启babel编译缓存
+                                    cacheCompression: false, // 缓存文件不要压缩
+                                },
+                            }
+                        ]
+                    }
+                ]
             }
         ]
     },
 
     plugins: [
         new ESLintPlugin({
-            context:path.resolve(__dirname,'../src')
+            context: path.resolve(__dirname, '../src'),
+            exclude:'node_modules',
+            threads, // 开启多进程
+
         }),
         new HtmlWebpackPlugin({
             // 以 public/index.html 为模板创建文件
@@ -75,5 +99,6 @@ module.exports = {
         open: true, // 是否自动打开浏览器
     },
 
-    mode: 'development'
+    mode: 'development',
+    devtool: "cheap-module-source-map"
 }
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,73 +1,131 @@
 const path = require('path')
 const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const os = require('os')
+// cpu核数
+const threads = os.cpus().length;
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+function getStyleLoader(pre) {
+    return [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        {
+            loader: "postcss-loader",
+            options: {
+                postcssOptions: {
+                    plugins: [
+                        "postcss-preset-env", // 能解决大多数样式兼容性问题
+                    ],
+                },
+            },
+        },
+        pre,
+    ].filter(Boolean);
+}
 
 module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, "../dist"),   //所有打包文件的输出路径
         filename: 'js/main.js',  //入口文件打包输出的文件名
-        clean:true   //自动清理dist目录
+        clean: true   //自动清理dist目录
     },
 
     // 加载器（lodaer）
     module: {
         rules: [
             {
-                // 用来匹配 .css 结尾的文件
-                test: /\.css$/,
-                // use 数组里面 Loader 执行顺序是从右到左
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader',
-                ],
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-            {
-                test: /\.(png|jpg?g|gif|webp|svg)$/,
-                type: 'asset',
-                parser: {
-                 dataUrlCondition: {
-                   maxSize: 20 * 1024 // 小于20kb，转base64  
-                 }
-                },
-                generator: {
-                    filename: 'image/[hash:10][ext][query]'   //hash:10  取哈希值前10位
-                }
-            },
-            {
-                test: /\.(ttf|woff2?|map3|map4|avi)$/,
-                type: 'asset/resource',    //纯输出这些资源，不需要做转换
-                generator: {
-                    filename: 'media/[hash:10][ext][query]'   //hash:10  取哈希值前10位
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/, // 排除node_modules代码不编译
-                loader: "babel-loader",
+                oneOf: [
+                    {
+                        // 用来匹配 .css 结尾的文件
+                        test: /\.css$/,
+                        // use 数组里面 Loader 执行顺序是从右到左
+                        use: getStyleLoader()
+                    },
+                    {
+                        test: /\.less$/,
+                        use: getStyleLoader('less-loader')
+                    },
+                    {
+                        test: /\.s[ac]ss$/,
+                        use: getStyleLoader('sass-loader')
+                    },
+                    {
+                        test: /\.(png|jpg?g|gif|webp|svg)$/,
+                        type: 'asset',
+                        parser: {
+                            dataUrlCondition: {
+                                maxSize: 20 * 1024 // 小于20kb，转base64  
+                            }
+                        },
+                        generator: {
+                            filename: 'image/[hash:10][ext][query]'   //hash:10  取哈希值前10位
+                        }
+                    },
+                    {
+                        test: /\.(ttf|woff2?|map3|map4|avi)$/,
+                        type: 'asset/resource',    //纯输出这些资源，不需要做转换
+                        generator: {
+                            filename: 'media/[hash:10][ext][query]'   //hash:10  取哈希值前10位
+                        }
+                    },
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/, // 排除node_modules代码不编译
+                        use:[
+                            {
+                                loader:'thread-loader',
+                                options:{
+                                    works:threads
+                                }
+                            },
+                            {
+                                loader: "babel-loader",
+                                options: {
+                                    cacheDirectory: true, // 开启babel编译缓存
+                                    cacheCompression: false, // 缓存文件不要压缩
+                                },
+                            }
+                        ]
+                        
+                    }
+                ]
             }
         ]
     },
 
     plugins: [
         new ESLintPlugin({
-            context:path.resolve(__dirname,'../src')
+            context: path.resolve(__dirname, '../src'),
+            exclude: 'node_modules',
+            cache: true,
+            // 缓存目录
+            cacheLocation: path.resolve(
+                __dirname,
+                "../node_modules/.cache/.eslintcache"
+            ),
+            threads, // 开启多进程
         }),
         new HtmlWebpackPlugin({
             // 以 public/index.html 为模板创建文件
             // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
             template: path.resolve(__dirname, "../public/index.html"),
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/main.css'
+        }),
     ],
-
-    mode: 'production'
+    optimization:{
+        minimizer:[
+            new CssMinimizerPlugin(),
+            new TerserWebpackPlugin({
+                parallel: threads // 开启多进程
+            })
+        ]
+    },
+    mode: 'production',
+    devtool: "source-map"
 }
